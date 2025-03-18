5e83071878e58f2ddb48e0bfb8f71d737343a9e5
xkzhai <xkzhai@ser.com>
2025-03-18 14:18:09 +0800

    todo islandesgap

---
+++ chap3/IslandEsgap.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int m,s,t;
+    cin>>m>>s>>t;
+    int dis = 0;
+    // 按秒循环
+    for(int i=1;i<=t && dis<s ;i++){
+        if(m>=10){// 魔法值够闪现，立刻闪
+            dis += 60;
+            m -= 10;
+        }else{// 魔法值不够闪，看剩余时间和剩余距离
+            if(m<2 && (t-i <= 6 || s-dis <= 102) 
+                || m<6 && (t-i <= 2 || s-dis <= 34)
+                || m<10 && (t-i<=1 || s-dis <= 17))
+                dis += 17;
+            else{
+                m += 4; // 原地休息，回复魔法值
+            }
+        }
+        if(dis>=s){
+            cout<<"Yes: "<<i<<"s"<<endl;
+            break;
+        }
+    }
+    if(dis<s){
+        cout<< "No: "<<dis<<endl;
+    }
+}
